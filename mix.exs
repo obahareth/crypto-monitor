dce395b2ecf9eacd1f9fb233446fc0e43f973855
Omar Bahareth <omar@omar.engineer>
2017-09-07 15:52:04 +0300

    Add poision, httpoision, and exfmt deps

--- mix.exs
+++ mix.exs
@@ -40,7 +40,10 @@ defmodule CryptoMonitor.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:poison, "~> 3.1"},
+      {:httpoison, "~> 0.13"},
+      {:exfmt, [github: "lpil/exfmt"]}
     ]
   end
 
